e1c004da8037cb0b53fff023d9cfb67ada8304f1
ericma <924402086@qq.com>
2020-04-10 14:52:09 +0800

    test husky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class FlexBox extends Component {
   render() {
