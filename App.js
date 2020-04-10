db1016efb2303cd1689347399883146557c105f5
ericma <924402086@qq.com>
2020-04-10 13:55:56 +0800

    flexDirection

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const styles = StyleSheet.create({
-  body: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-});
-
-export default class HelloWorldApp extends Component {
+export default class FlexBox extends Component {
   render() {
     return (
-      <View style={styles.body}>
-        <Text>Hello, world!</Text>
+      <View style={styles.container}>
+        <View style={styles.view1} />
+        <View style={styles.view2} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: 'row',// flexDirection 默认是 column
+    backgroundColor: 'black',
+  },
+  view1: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'red',
+  },
+  view2: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'green',
+  },
+});
