68b23bcb0bdc25dc0cab8fc9b649f3193ff0b704
ericma <924402086@qq.com>
2020-04-10 15:40:34 +0800

    add search bar ui

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class FlexBox extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.view1} />
-        <View style={styles.view2} />
-      </View>
+      <>
+        <View style={styles.search}>
+          <TextInput style={styles.input} placeholder="搜索商品" />
+          <Button style={styles.button} title="搜索" />
+        </View>
+        <View style={styles.advertisement}>
+          <Text>轮播广告</Text>
+        </View>
+        <View style={styles.products}>
+          <Text>商品列表</Text>
+        </View>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
   // ctrl + m 打开调试菜单
-  container: {
-    flex: 1,
-    // flexWrap: 'wrap', // 是因为换行导致的垂直排列 (又这个样式就不能垂直居中了)
-    // flexDirection: 'row', // flexDirection 默认是 column
-    backgroundColor: 'gray',
-    // justifyContent: 'center', // 内容水平居中
-    // alignItems: 'center', // 内容垂直居中
-  },
-  view1: {
-    flex: 1,
+  //   container: {
+  //     flex: 1,
+  //     // flexWrap: 'wrap', // 是因为换行导致的垂直排列 (又这个样式就不能垂直居中了)
+  //     // flexDirection: 'row', // flexDirection 默认是 column
+  //     backgroundColor: 'gray',
+  //     // justifyContent: 'center', // 内容水平居中
+  //     // alignItems: 'center', // 内容垂直居中
+  //   },
+  search: {
+    marginTop: Platform.OS === 'android' ? 0 : 20, // Platform 平台的信息
+    height: 40,
     backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  view2: {
+  input: {
     flex: 1,
+    backgroundColor: 'white',
+    borderWidth: 2,
+  },
+  button: {
+    flex: 1,
+  },
+  advertisement: {
+    height: 180,
     backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  products: {
+    flex: 1,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
