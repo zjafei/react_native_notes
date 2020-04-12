bfb3228eb57540fcc4f2e4e453463971718c4dcf
ericma <924402086@qq.com>
2020-04-12 14:25:52 +0800

    indicator

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ import {
   Alert,
   TouchableHighlight,
   StatusBar,
+  Image,
 } from 'react-native';
+
+const circleSize = 8;
+const circleMargin = 5;
 
 export default class FlexBox extends Component {
   constructor(props) {
@@ -55,14 +59,17 @@ export default class FlexBox extends Component {
         {
           title: 'ad 01',
           backgroundColor: 'yellow',
+          url: 'https://hbimg.huabanimg.com/c8e5558c493e3ac04936eb111946ba85a5751873dd156-r8Vr0M_fw658',
         },
         {
           title: 'ad 02',
           backgroundColor: 'red',
+          url: 'https://hbimg.huabanimg.com/f7b0d94e4f0658aa817d01f694e92b3bdf3edec314da75-zdyHkQ_fw658',
         },
         {
           title: 'ad 03',
           backgroundColor: 'green',
+          url: 'https://hbimg.huabanimg.com/afbd40bb8f2d926eea914d540bcb3df32cc1f2e993649-o0ErFs_fw658',
         },
       ],
       searchText: '',
@@ -113,6 +120,7 @@ export default class FlexBox extends Component {
             placeholder="搜索商品"
             value={this.state.searchText}
             onChangeText={(text) => {
+              console.log(this.state.searchText);
               this.setState({
                 searchText: text,
               });
@@ -122,7 +130,7 @@ export default class FlexBox extends Component {
             style={styles.button}
             title="搜索"
             onPress={() => {
-              Alert.alert(`搜索: ${this.state.searchText}`);
+              Alert.alert(`搜索内容: ${this.state.searchText}`);
             }}
           />
         </View>
@@ -142,19 +150,15 @@ export default class FlexBox extends Component {
                   Alert.alert(`你点了${item.title}`);
                 }}
               >
-                <Text
-                  style={[
-                    styles.advertisementItem,
-                    {
-                      backgroundColor: item.backgroundColor,
-                    },
-                  ]}
-                >
-                  {index}-{item.title}
-                </Text>
+                <Image style={styles.advertisementItem} source={{ uri: item.url }} />
               </TouchableHighlight>
             ))}
           </ScrollView>
+          <View style={styles.indicator}>
+            {this.state.advertisements.map((_, index) => (
+              <View key={index} style={[styles.circle, index === this.state.currentPage ? styles.circleCur : {}]} />
+            ))}
+          </View>
         </View>
         <View style={styles.products}>
           <FlatList data={this.state.dataSource} renderItem={this._renderRow} />
@@ -177,7 +181,7 @@ const styles = StyleSheet.create({
   search: {
     marginTop: Platform.OS === 'android' ? 0 : 20, // Platform 平台的信息
     height: 40,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
@@ -185,7 +189,8 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     backgroundColor: 'white',
-    // borderWidth: 2,
+    borderWidth: 2,
+    borderRadius: 10,
   },
   button: {
     flex: 1,
@@ -206,5 +211,23 @@ const styles = StyleSheet.create({
     height: 60,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  indicator: {
+    position: 'absolute',
+    top: 160,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    left: 0,
+    right: 0,
+  },
+  circle: {
+    width: circleSize,
+    height: circleSize,
+    borderRadius: circleSize / 2,
+    backgroundColor: 'white',
+    marginHorizontal: circleMargin,
+  },
+  circleCur: {
+    backgroundColor: 'orange',
   },
 });
