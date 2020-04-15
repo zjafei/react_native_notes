d389ea5ce8b29320c261a718ac7b3820e6138379
ericma <924402086@qq.com>
2020-04-15 15:26:48 +0800

    add @react-navigation/stack and app can running

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,41 +1,19 @@
-// import React, { Component } from 'react';
-// import Home from './Home';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { Component } from 'react';
+import Home from './Home';
 
-// export default class Page extends Component {
-//   render() {
-//     return <Home />;
-//   }
-// }
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
+const Stack = createStackNavigator();
 
-const HomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen</Text>
-    <Button onPress={() => navigation.navigate('Details')} title="Go to details" />
-  </View>
-);
-
-const DetailsScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Details Screen</Text>
-  </View>
-);
-
-const RootNavigator = StackNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      headerTitle: 'Home',
-    },
-  },
-  Details: {
-    screen: DetailsScreen,
-    navigationOptions: {
-      headerTitle: 'Details',
-    },
-  },
-});
-
-export default RootNavigator;
+export default class Page extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
